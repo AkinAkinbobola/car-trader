d478c7137538e807e517712a029baee41f0b291d
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-26 11:41:56 +0100

    Add custom.vue layout

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -4,15 +4,14 @@ const route = useRoute()
 useHead({
   title: `${route.params.make ? useSentenceCase(route.params.make) : 'Cars'} in ${useSentenceCase(route.params.city)}`
 })
+definePageMeta({
+  layout: 'custom'
+})
 </script>
 
 <template>
-  <div
-      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16"
-  >
-    <div class="mt-32 flex">
-      <CarSidebar/>
-      <NuxtPage/>
-    </div>
+  <div class="mt-32 flex">
+    <CarSidebar/>
+    <NuxtPage/>
   </div>
 </template>
