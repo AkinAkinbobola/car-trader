980c6fdb1aa9cbf63f13ba94977d0aa64a2658c2
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-26 12:42:55 +0100

    Add car data to details

--- components/Car/Detail/Attributes.vue
+++ components/Car/Detail/Attributes.vue
@@ -4,7 +4,7 @@ const {features} = defineProps(['features'])
 
 <template>
   <div class="mr-10 mt-5 border-b pb-5">
-    <div class="flex text-lg mt-2" v-for="feature in features">
+    <div class="flex text-lg mt-2" v-for="feature in features" :key="feature">
       <p class="rounded text-lime-800 mr-3">✔</p>
       <p>{{ feature }}</p>
     </div>
