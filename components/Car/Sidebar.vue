c1525df85228cb71ce16a3307f29c9eded2e4c5f
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-28 11:53:49 +0100

    Add makes filter

--- components/Car/Sidebar.vue
+++ components/Car/Sidebar.vue
@@ -1,4 +1,5 @@
 <script setup>
+const {makes} = useMakes();
 const route = useRoute();
 const city = ref('');
 const modal = ref({
@@ -12,8 +13,8 @@ const updateModal = (key) => {
 }
 
 const onChangeLocation = () => {
-  if(!city.value) return;
-  if(!isNaN(parseInt(city.value))) {
+  if (!city.value) return;
+  if (!isNaN(parseInt(city.value))) {
     throw createError({
       statusCode: 400,
       statusMessage: 'City cannot be a number'
@@ -23,13 +24,18 @@ const onChangeLocation = () => {
   navigateTo(`/city/${city.value}/car/${route.params.make}`);
   city.value = '';
 }
+
+const onChangeMake = (make) => {
+  updateModal('make');
+  navigateTo(`/city/${route.params.city}/car/${make}`);
+}
 </script>
 
 <template>
   <div class="shadow border w-[40%] mr-10 z-30 h-[190px]">
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
-      <h3 class="text-blue-400 capitalize" @click="updateModal('location')">{{route.params.city}}</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('location')">{{ route.params.city }}</h3>
       <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white" v-if="modal.location">
         <input type="text" class="border p-1 rounded" v-model="city"/>
         <button class="bg-blue-400 w-full mt-2 rounded text-white p-1" @click="onChangeLocation">
@@ -40,7 +46,16 @@ const onChangeLocation = () => {
 
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
-      <h3 class="text-blue-400 capitalize">Toyota</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('make')">
+        {{route.params.make ? route.params.make : 'Any'}}
+      </h3>
+      <div
+          v-if="modal.make"
+          class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white">
+        <h4 v-for="make in makes" :key="make.id" class="w-1/3" @click="onChangeMake(make)">
+          {{ make }}
+        </h4>
+      </div>
     </div>
 
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
