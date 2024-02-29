a7bdce6aee71330955b3168922796269b46af77e
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-29 09:23:57 +0100

    Add logout

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,15 @@
 <script setup>
-const user = ref(false)
+const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+
+const logout = async () => {
+  const {error} = await supabase.auth.signOut()
+  if(error){
+    return
+  }
+  user.value = null
+  navigateTo('/logout')
+}
 </script>
 
 <template>
@@ -7,14 +17,14 @@ const user = ref(false)
       class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
   >
     <NuxtLink class="text-3xl font-mono" to="/">cartrader</NuxtLink>
-<!--    <div>-->
-<!--      <div class="flex items-center justify-center" v-if="user">-->
-<!--        <NuxtLink>Profile</NuxtLink>-->
-<!--        <NuxtLink>Logout</NuxtLink>-->
-<!--      </div>-->
-<!--      <div v-else>-->
-<!--        <NuxtLink>Login</NuxtLink>-->
-<!--      </div>-->
-<!--    </div>-->
+    <div>
+      <div class="flex items-center justify-center gap-5" v-if="user">
+        <NuxtLink to="/profile/listings">Profile</NuxtLink>
+        <p class="cursor-pointer" @click="logout">Logout</p>
+      </div>
+      <div v-else>
+        <NuxtLink to="/login">Login</NuxtLink>
+      </div>
+    </div>
   </header>
 </template>
