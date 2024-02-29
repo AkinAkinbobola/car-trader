ec95cf2b0427e7b43449f4703b4bf16bc0f5a562
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-29 09:05:29 +0100

    Add OAuth

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,12 +1,20 @@
+<script setup>
+const user = ref(false)
+</script>
+
 <template>
   <header
       class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
   >
     <NuxtLink class="text-3xl font-mono" to="/">cartrader</NuxtLink>
+<!--    <div>-->
+<!--      <div class="flex items-center justify-center" v-if="user">-->
+<!--        <NuxtLink>Profile</NuxtLink>-->
+<!--        <NuxtLink>Logout</NuxtLink>-->
+<!--      </div>-->
+<!--      <div v-else>-->
+<!--        <NuxtLink>Login</NuxtLink>-->
+<!--      </div>-->
+<!--    </div>-->
   </header>
 </template>
-<script>
-export default {
-  name: 'NavBar'
-}
-</script>
---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<script setup>
+definePageMeta({
+  layout: 'custom'
+})
+
+const supabase = useSupabaseClient()
+
+const login = async () => {
+  const {error} = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: 'http://localhost:3000/confirm'
+    }
+  })
+  if(error){
+    console.log(error)
+  }
+}
+</script>
+
+<template>
+  <div class="mt-10">
+    <h1 class="text-5xl font-bold mb-7">Log In</h1>
+    <button class="bg-red-400 p-3 rounded text-white font-bold" @click="login">Login with Google</button>
+  </div>
+</template>
