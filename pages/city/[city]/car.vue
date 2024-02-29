cba307e6ae876b96b94f029d316d9e995f766860
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-29 11:16:17 +0100

    Remove unnecessary layout

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -5,9 +5,6 @@ const {toTitleCase} = useUtilities()
 useHead({
   title: `${route.params.make ? toTitleCase(route.params.make) : 'Cars'} in ${toTitleCase(route.params.city)}`
 })
-definePageMeta({
-  layout: 'custom'
-})
 </script>
 
 <template>
