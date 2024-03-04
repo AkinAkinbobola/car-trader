30236773a8d800a10f89d31bc1d89ba0f7841c2a
Akin Akinbobola <akinbobola71@gmail.com>
2024-03-04 07:48:17 +0100

    Create car objects from client

--- pages/profile/listings/create.vue
+++ pages/profile/listings/create.vue
@@ -1,11 +1,12 @@
 <script setup>
-
 definePageMeta({
   layout: "custom",
 });
 
-const { makes } = useData();
+const {makes} = useData();
 
+const user = useSupabaseUser()
+const errorMessage = ref('')
 const info = useState("adInfo", () => {
   return {
     make: "",
@@ -17,7 +18,7 @@ const info = useState("adInfo", () => {
     seats: "",
     features: "",
     description: "",
-    image: null,
+    image: "sdvdsvjksvnsd",
   };
 });
 
@@ -40,6 +41,12 @@ const inputs = [
   },
   {
     id: 3,
+    title: "Price *",
+    name: "price",
+    placeholder: "10000",
+  },
+  {
+    id: 4,
     title: "Miles *",
     name: "miles",
     placeholder: "10000",
@@ -51,18 +58,50 @@ const inputs = [
     placeholder: "Austin",
   },
   {
-    id: 5,
+    id: 6,
     title: "Number of Seats *",
     name: "seats",
     placeholder: "5",
   },
   {
-    id: 6,
+    id: 7,
     title: "Features *",
     name: "features",
     placeholder: "Leather Interior, No Accidents",
   },
 ];
+
+const isButtonDisabled = computed(() => {
+  for (let key in info.value) {
+    if (info.value[key] === "") {
+      return true
+    }
+  }
+  return false
+})
+const handleSubmit = async () => {
+    const body = {
+      ...info.value,
+      features: info.value.features.split(", "),
+      numberOfSeats: parseInt(info.value.seats),
+      year: parseInt(info.value.year),
+      miles: parseInt(info.value.miles),
+      price: parseInt(info.value.price),
+      listerId: user.value.id,
+      name: info.value.make + " " + info.value.model,
+      image: "sdvdsvjksvnsd"
+    }
+    delete body.seats
+    try{
+        const response = await $fetch('/api/car/listings', {
+          method: 'POST',
+          body
+        })
+      navigateTo('/profile/listings')
+    }catch (e) {
+      errorMessage.value = e.value.statusMessage
+    }
+}
 </script>
 
 
@@ -73,26 +112,37 @@ const inputs = [
     </div>
     <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
       <CarAdSelect
-        title="Make *"
-        :options="makes"
-        name="make"
-        @change-input="onChangeInput"
+          title="Make *"
+          :options="makes"
+          name="make"
+          @change-input="onChangeInput"
       />
       <CarAdInput
-        v-for="input in inputs"
-        :key="input.id"
-        :title="input.title"
-        :name="input.name"
-        :placeholder="input.placeholder"
-        @change-input="onChangeInput"
+          v-for="input in inputs"
+          :key="input.id"
+          :title="input.title"
+          :name="input.name"
+          :placeholder="input.placeholder"
+          @change-input="onChangeInput"
       />
       <CarAdTextarea
-        title="Description *"
-        name="description"
-        placeholder=""
-        @change-input="onChangeInput"
+          title="Description *"
+          name="description"
+          placeholder=""
+          @change-input="onChangeInput"
       />
-      <CarAdImage @change-input="onChangeInput" />
+      <CarAdImage @change-input="onChangeInput"/>
+      <div>
+        <button
+            @click="handleSubmit"
+            :disabled="isButtonDisabled"
+            :class="isButtonDisabled ? 'bg-gray-400' : 'bg-blue-400'"
+            class="bg-blue-400 text-white rounded py-2 px-7 mt-2">Submit
+        </button>
+      </div>
+      <p v-if="errorMessage" class="mt-3 text-red-400">
+        {{errorMessage}}
+      </p>
     </div>
   </div>
 </template>
